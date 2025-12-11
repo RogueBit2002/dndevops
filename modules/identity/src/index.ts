45ed7daece52c3c3c0f6ebce6d66e2db536156d4
Laurens Kruis <laurens@kruis.name>
2025-12-11 22:54:58 +0100

    commit before rework

--- modules/identity/src/index.ts
+++ modules/identity/src/index.ts
@@ -1,14 +1,213 @@
-import { Effect, Data, Context} from "effect";
+import * as PgClient from "@effect/sql-pg/PgClient";
 
-import { Principal } from "@dndevops/library-identity";
+import { Data, Effect, Context, Random, DateTime, Layer, Redacted, Schedule, Duration } from "effect";
 
-import { InvalidPermissionsError } from "@dndevops/library-errors";
+import { createTransport } from "nodemailer";
 
-export class MissingUserError extends Data.TaggedError("MissingUserError")<{}> {}
-export class MissingTeamError extends Data.TaggedError("MissingTeamError")<{}> {}
-export class DuplicateUserError extends Data.TaggedError("DuplicateUserError")<{}> {}
-export class DuplicateTeamError extends Data.TaggedError("DuplicationTeamTerror")<{}> {}
+import * as jwt from "jsonwebtoken";
 
+export class MissingUserError extends Data.TaggedError("MissingUserError")<{}> {};
+export class MissingTeamError extends Data.TaggedError("MissingTeamError")<{}> {};
+export class DuplicateUserError extends Data.TaggedError("DuplicateUserError")<{}> {};
+export class DuplicateTeamError extends Data.TaggedError("DuplicationTeamTerror")<{}> {};
+export class UnauthorizedError extends Data.TaggedError("UnauthorizedError")<{}>{};
+let _nonce: string | undefined = undefined;
+
+
+
+const PgLive = Layer.unwrapEffect(
+	Effect.gen(function*() {
+		return PgClient.layer({
+			url: Redacted.make("dndevops-identity-db"),
+		})
+	})
+).pipe((self) =>
+  Layer.retry(
+    self,
+    Schedule.identity<Layer.Layer.Error<typeof self>>().pipe(
+      Schedule.check((input) => input._tag === "SqlError"),
+      Schedule.intersect(Schedule.exponential("1 second")),
+      Schedule.intersect(Schedule.recurs(2)),
+      Schedule.onDecision(([[_error, duration], attempt], decision) =>
+        decision._tag === "Continue"
+          ? Effect.logInfo(
+              `Retrying database connection in ${Duration.format(duration)} (attempt #${++attempt})`,
+            )
+          : Effect.void,
+      ),
+    ),
+  ),
+);
+
+class IdentityDataService extends Effect.Service<IdentityDataService>()("@dndevops/IdentityDataService", {
+	dependencies: [ ],
+	effect: Effect.gen(function*() {
+
+		//Create database connection
+
+		return {
+			userExists: (email: string) => Effect.gen(function*() {
+				if(email == "laurens@kruis.name")
+					return true;
+
+				return false;
+			}),
+
+			storeRefreshNonce: (email: string, nonce: string, expiresAt: DateTime.Utc) => Effect.gen(function*() {
+				_nonce = nonce;
+			}),
+
+			getRefreshNonce: (email: string) => Effect.gen<never, string | undefined>(function*() {
+				return  _nonce;
+			}),
+
+			storeRefreshToken: (email: string, token: string) => Effect.gen(function*() {
+
+			}),
+
+			/*validateRefreshTokens: (email: string, token: string) => Effect.gen(function*() {
+
+			}),*/
+
+			getRefreshTokens: (email: string) => Effect.gen(function*() {
+				return ["", ""];
+			}),
+
+			assignUserToTeam: (email: string, team: string) => Effect.gen(function*() {
+
+			}),
+
+			removeUserFromTeam: (email: string, team: string) => Effect.gen(function*() {
+				
+			}),
+
+			createTeam: (displayName: string) => Effect.gen(function*() {
+
+			}),
+
+			deleteTeam: (id: string) => Effect.gen(function*() {
+
+			}),
+
+			updateTeam: (id: string, displayName: string) => Effect.gen(function*() {
+
+			}),
+
+			getTeam: (id: string) => Effect.gen(function*() {
+
+			}),
+
+			getTeamsByUser: (email: string) => Effect.gen(function*() {
+				return ["aaa", "bbb"];
+			})
+		}
+	})
+}){};
+
+export class MailService extends Effect.Service<MailService>()("@dndevops/MailService", {
+	effect: Effect.gen(function*() {
+		const mailer = createTransport({
+			host: "mailpit",
+			port: 1025 ,
+			secure: false, // true for 465, false for other ports
+			auth: {
+				user: "login@doesnt.matter",
+				pass: "beepboop",
+			},
+		});
+
+
+		return (email: string, subject: string, message: string) => Effect.promise(async () => {
+			await mailer.sendMail({
+				from: '"NAME" <dndevops@company.com>',
+				to: email,
+				subject: subject,
+				text: message, // plain‑text body
+			});
+		});
+	})
+}){};
+
+
+export class IdentityService extends Effect.Service<IdentityService>()("@dndevops/IdentityService", {
+	dependencies: [ IdentityDataService.Default, MailService.Default ],
+	effect: Effect.gen(function*() {
+		const data = yield* IdentityDataService;
+
+		const sendMail = yield* MailService;
+
+		return {
+			requestRefreshToken: (email: string) => Effect.gen(function*() {
+				const exists = yield* data.userExists(email);
+
+				if(!exists)
+					return yield* Effect.fail(new MissingUserError());
+
+				const digits = Array.from({ length: 5}, () => Math.floor(Math.random() * 10))
+				const nonce = digits.join("");
+
+				const now = yield* DateTime.now;
+				const expiresAt = DateTime.add(now, { minutes: 10});
+
+				yield* data.storeRefreshNonce(email, nonce, expiresAt);
+
+				yield* sendMail(email, "DNDevOps Login", nonce);
+			}),
+
+			getRefreshToken: (email: string, nonce: string) => Effect.gen(function*() {
+				const n = yield* data.getRefreshNonce(email);
+
+				if (n != nonce)
+					return yield* new UnauthorizedError;
+
+				//Make refresh token
+				const refreshToken = "xxx";
+
+				yield* data.storeRefreshToken(email, refreshToken);
+				return refreshToken;
+			}),
+
+			getAccessToken: (refreshToken: string) => Effect.gen(function*() {
+
+				let decoded: {[key: string]: any};
+				try {
+					decoded = jwt.verify(refreshToken, 'shhhhh') as any;
+				} catch {
+					return yield* new UnauthorizedError;
+				}
+
+				const email = decoded.email;
+
+				const tokens = yield* data.getRefreshTokens(email);
+
+				if(!tokens.includes(refreshToken))
+					return yield* new UnauthorizedError;
+
+				const teams = yield* data.getTeamsByUser(email);
+				const now = yield* DateTime.now;
+
+				const payload = {
+					email,
+					exp: DateTime.add(now, { minutes: 10 }),
+					teams
+				};
+
+				const accessToken = "yyy";
+
+				return accessToken;
+			}),
+			/*
+				createTeam
+				deleteTeam
+				updateTeam
+				getTeam
+			*/
+		};
+	})
+}){};
+
+
+/*
 class IdentityStorageService extends Context.Tag("IdentityStorageService")<
 	IdentityStorageService, 
 	{
@@ -55,4 +254,4 @@ export class IdentityService extends Context.Tag("IdentityService")<
 			throw new Error("Function not implemented.");
 		}
 	});
-};
+};*/
